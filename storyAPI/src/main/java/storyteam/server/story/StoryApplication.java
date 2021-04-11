6fcd4eec223814ff7f649cf14579b18d9cee8cdf
GET MOUSSED <mofanafoussa@gmail.com>
2021-04-11 15:14:43 +0200

    removed package json in root directory

--- storyAPI/src/main/java/storyteam/server/story/StoryApplication.java
+++ storyAPI/src/main/java/storyteam/server/story/StoryApplication.java
@@ -3,11 +3,9 @@ package storyteam.server.story;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "storyteam.server.story")
 public class StoryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StoryApplication.class, args);
