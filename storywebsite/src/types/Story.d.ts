9f233dd08df30233fae73e5c07ff5982bfbf6b98
GET MOUSSED <mofanafoussa@gmail.com>
2021-09-02 19:31:03 +0200

    removed unused types

--- storywebsite/src/types/Story.d.ts
+++ storywebsite/src/types/Story.d.ts
@@ -1,10 +1,13 @@
+import Comment from "./Comment";
 import User from "./User";
+
 export default interface Story {
 	id: number;
 	name: string;
 	description: string;
 	creationDate: string;
 	user: User;
-	firstBlocId: number;
+	firstBlocId: string;
+	comments: Comment[];
 	cover: string;
 }
